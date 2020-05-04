c96a44626a35bece9a5a27ee57f680e5ea693a7b
drmeph <kevin.dorfer@gmail.com>
2020-05-04 09:35:13 -0400

    week 4: Combining reducers

--- confusion/src/redux/configureStore.js
+++ confusion/src/redux/configureStore.js
@@ -1,11 +1,20 @@
-import { createStore } from "redux";
-import { Reducer, initalState} from "./reducer";
+import { createStore, combineReducers } from "redux";
+import {Dishes} from "./dishes";
+import {Comments} from "./comments";
+import {Promotions} from "./promotions";
+import {Leaders} from "./leaders";
+
 
 export const ConfigureStore = () => {
 
     const store = createStore(
-        Reducer,
-        initalState
+        combineReducers({
+            dishes: Dishes,
+            comments: Comments,
+            promotions: Promotions,
+            leaders: Leaders
+        })
+
     );
 
     return store;
